c91b6b9e5ab760bd71de1192155dd6a05288a97a
pedrojuarezr <pj855583@gmail.com>
2023-09-21 10:29:53 -0600

    Mi primer Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import firebase from "firebase/app";
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+const firebaseConfig = {
+  apiKey: "AIzaSyBqOQ77cHhGOGsvWPTKVUFmR8IY5kySR-0",
+  authDomain: "aplicaciones-web-7e040.firebaseapp.com",
+  projectId: "aplicaciones-web-7e040",
+  storageBucket: "aplicaciones-web-7e040.appspot.com",
+  messagingSenderId: "683753864855",
+  appId: "1:683753864855:web:dae0ef191ec02d5cf3b3d2"
+};
+firebase.initializeApp(firebaseConfig);
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase/app';
+import 'firebase/database';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  direccion:any;
+  nombre:string = '';
+  colonia:any;
   constructor() {}
 
+async guardarformulario(){
+  const databaseRef = firebase.database().ref('/formulario');
+  const datos = {
+    direccion: this.direccion,
+    nombre: this.nombre,
+    colonia: this.colonia,
+  };
+  databaseRef.child(this.nombre).set(datos).then(()=>{
+    console.log('subido exitosamnete!');
+  })
 }
+}
+
